9efa9ba20644c9c5bf3b92e6e03449d7a021eeb2
Egor Iutkin <iutkin-ed@mail.ru>
2022-05-12 20:05:23 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dynamic_component-drag_and_drop';
+}
